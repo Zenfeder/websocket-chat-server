214371c77a23acd830aabc3cbac5979692a9a344
CG <1054609489@qq.com>
2018-08-05 10:12:38 +0800

    【update】添加邀请私聊与私聊功能

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ io.on('connection', socket => {
       console.log(`[disconnect] ${socketCache[index].nickname} 下车了`)
       socket.broadcast.emit('user exit broadcast', socketCache[index].nickname)
       socketCache.splice(index, 1)
-      io.sockets.emit('get all connected nickname', socketCache.map(elem => elem.nickname))
+      io.sockets.emit('push connected user list', socketCache.map(elem => elem.nickname))
     }
     console.log(socketCache)
   })
-
   socket.on('join broadcast', nickname => {
     let index = socketCache.findIndex(elem => elem.nickname === nickname)
     if (index === -1) {
@@ -31,13 +30,12 @@ io.on('connection', socket => {
       socketCache.push({ nickname: nickname, socketId: socket.id })
       io.to(socket.id).emit('self join response', { success: true, message: '' })
       socket.broadcast.emit('user join broadcast', nickname)
-      io.sockets.emit('get all connected nickname', socketCache.map(elem => elem.nickname))
+      io.sockets.emit('push connected user list', socketCache.map(elem => elem.nickname))
     } else {
       io.to(socket.id).emit('self join response', { success: false, message: '用户名已被占用' })
     }
     console.log(socketCache)
   })
-
   socket.on('join reconnect', nickname => {
     let index = socketCache.findIndex(elem => elem.nickname === nickname)
     if (index === -1) {
@@ -45,31 +43,41 @@ io.on('connection', socket => {
       socketCache.push({ nickname: nickname, socketId: socket.id })
       io.to(socket.id).emit('self reconnect response', { success: true, message: '已为你重新建立连接' })
       socket.broadcast.emit('user reconnected', nickname)
-      io.sockets.emit('get all connected nickname', socketCache.map(elem => elem.nickname))
+      io.sockets.emit('push connected user list', socketCache.map(elem => elem.nickname))
     } else {
       io.to(socket.id).emit('self reconnect response', { success: false, message: '用户名已被占用' })
     }
     console.log(socketCache)
   })
-
-  socket.on('typing broadcast message', nickname => {
-    console.log(`[typing broadcast message] ${nickname} 正在输入...`)
-    socket.broadcast.emit('user typing broadcast message', nickname)
+  socket.on('pull connected user list', () => {
+    console.log(`[pull connected user list]`)
+    io.sockets.emit('push connected user list', socketCache.map(elem => elem.nickname))
   })
-
   socket.on('broadcast message', ({ nickname, message }) => {
     console.log(`[broadcast message] ${nickname} 广播消息：${message}`)
     socket.broadcast.emit('user broadcast message', { nickname, message })
   })
-
-  // socket.on('privateMsg', ({ fromNickname, toSocketId, message }) => {
-  //   console.log(`[privateMsg] ${fromNickname} said to ${toSocketId}: ${message}`)
-  //   socket.broadcast.to(toSocketId).emit('user private msg', {
-  //     fromNickname: fromNickname,
-  //     fromSocketId: socket.id,
-  //     message: message
-  //   })
-  // })
+  socket.on('send private chat invite', ({ sendNickname, receiveNickname }) => {
+    let receiver = socketCache.find(elem => elem.nickname === receiveNickname)
+    console.log(`[send private chat invite] ${sendNickname} 邀请 ${receiveNickname} 私聊`)
+    if (receiver) {
+      io.to(receiver.socketId).emit('receive private chat invite', { sendNickname, receiveNickname })
+    }
+  })
+  socket.on('typing private message', ({ sendNickname, receiveNickname }) => {
+    let receiver = socketCache.find(elem => elem.nickname === receiveNickname)
+    console.log(`[typing private message] ${sendNickname} 正在输入...`)
+    if (receiver) {
+      io.to(receiver.socketId).emit('user typing private message')
+    }
+  })
+  socket.on('send private message', ({ sendNickname, receiveNickname, message }) => {
+    let receiver = socketCache.find(elem => elem.nickname === receiveNickname)
+    console.log(`[send private message] ${sendNickname} 对 ${receiveNickname} 说: ${message}`)
+    if (receiver) {
+      io.to(receiver.socketId).emit('receive private message', { sendNickname, message })
+    }
+  })
 })
 
 http.listen(8081, () => {
